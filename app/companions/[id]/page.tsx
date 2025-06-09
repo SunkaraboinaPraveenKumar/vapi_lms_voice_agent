c13bcfc2fdc3b984268a39791ae5a6be773e9c21
SunkaraboinaPraveenKumar <sunkaraboinap@gmail.com>
2025-06-09 23:28:22 +0530

    vapi connected and transcript

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getCompanion } from '@/lib/actions/companion.actions'; 
+import CompanionComponent from '@/components/CompanionComponent';
+import { getCompanion } from '@/lib/actions/companion.actions';
 import { getSubjectColor } from '@/lib/utils';
 import { currentUser } from '@clerk/nextjs/server';
 import Image from 'next/image';
@@ -8,25 +9,26 @@ import React from 'react'
 interface CompanionSessonPageProps {
   params: Promise<{ id: string }>;
 }
-const CompanionSession = async({ params }: CompanionSessonPageProps) => {
-  const {id}= await params;
-  const {name, subject, title, topic, duration} = await getCompanion(id);
+const CompanionSession = async ({ params }: CompanionSessonPageProps) => {
+  const { id } = await params;
+  const companion = await getCompanion(id);
+  const { name, subject, title, topic, duration } = companion;
   const user = await currentUser();
-  if(!user){
+  if (!user) {
     redirect('/sign-in');
   }
-  if(!name){
-    redirect("/companions")
+  if (!name) {
+    redirect("/companions");
   }
   return (
     <main>
       <article className='flex rounded-border justify-between p-6 max-md:flex-col'>
         <div className='flex items-center gap-2'>
-          <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{backgroundColor:getSubjectColor(subject)}}>
+          <div className='size-[72px] flex items-center justify-center rounded-lg max-md:hidden' style={{ backgroundColor: getSubjectColor(subject) }}>
             <Image src={`/icons/${subject}.svg`}
-            alt={subject}
-            width={35}
-            height={35}
+              alt={subject}
+              width={35}
+              height={35}
             />
           </div>
           <div className='flex flex-col gap-2'>
@@ -45,6 +47,13 @@ const CompanionSession = async({ params }: CompanionSessonPageProps) => {
           {duration} minutes
         </div>
       </article>
+
+      <CompanionComponent
+      {...companion}
+      companionId={id}
+      userName={user.firstName}
+      userImage={user.imageUrl}
+      />
     </main>
   )
 }
