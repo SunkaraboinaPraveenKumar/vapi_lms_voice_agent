433dbf795863b6bcd8008ffea09829e19183d6ef
SunkaraboinaPraveenKumar <sunkaraboinap@gmail.com>
2025-06-10 10:00:59 +0530

    my-journey page added

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -101,3 +101,17 @@ export const getUserSessions = async (limit = 10) => {
     }
     return data.map(({ companions }) => companions)
 }
+
+export const getUserCompanions = async (userId: string) => {
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('author', userId)
+        .order('created_at', { ascending: false })
+
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data;
+}
